811a3cf2235bf6c36934dab9add23965aaf714ab
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-22 18:08:01 +0100

    Initial steps for landing page completed

---
+++ src/Components/Home/home.js
@@ -0,0 +1,14 @@
+import './home.css';
+import pic from '../../Assets/Images/landpic.jpg';
+
+const Home = () => {
+    return (
+        <>
+        <div className='header '>
+            <img src={pic} alt='#' className='bg-img'/>
+        </div>
+        </>
+    )
+}
+
+export default Home;
