e4f07a786b8593d1f6fbb4a310e51d45c61e12d9
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-23 17:17:19 +0100

    navbar complete

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,13 +1,12 @@
 import './home.css';
-import pic from '../../Assets/Images/landpic.jpg';
+// import pic from '../../Assets/Images/landpic.jpg';
 import Navbar from '../Navbar/navbar';
 
 const Home = () => {
     return (
         <>
-        <div className='header '>
+        <div className='header'>
             <Navbar/>
-            <img src={pic} alt='#' className='bg-img'/>
         </div>
         </>
     )
--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,10 +1,20 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Logo from '../../Assets/Images/biglogo-png.png';
+import './navbar.css';
 
 const Navbar = () => {
 
   return(
-    <div>
-      <img />
+    <div className="head">
+      <img className='logo' src={Logo} alt="logo"/>
+      <nav>
+        <ul className="nav_links">
+          <li><Link className='a' aria-current='page' to='/'>HOME</Link></li>
+          <li><Link className='a' to='/about'>ABOUT</Link></li>
+          <li><Link className='a' to='/categories'>CATEGORIES</Link></li>
+        </ul>
+      </nav>
+      <Link className="nav_btn" to='/signup'><button>Get Started</button></Link>
     </div>
   )
 }
