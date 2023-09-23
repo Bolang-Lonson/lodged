dc74a89bd0d38cd8a4d463fc1ef93519762042c7
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-23 17:58:00 +0100

    landing page progress

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,12 +1,16 @@
 import './home.css';
-// import pic from '../../Assets/Images/landpic.jpg';
 import Navbar from '../Navbar/navbar';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <>
         <div className='header'>
             <Navbar/>
+            <div className='info'>
+                <h1>let's find <br></br>your <br></br>dream hotel room or <br></br>guest house</h1>
+                <Link className='s-u'><button>get started &rarr;</button></Link>
+            </div>
         </div>
         </>
     )
