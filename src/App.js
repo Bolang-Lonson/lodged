3a9b7e7e2279d9f1d394d3810c6c6308be9b1afa
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-23 18:04:31 +0100

    about page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/home';
+import About from './Components/About/about';
 
 const App = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
     </Routes>
     </>
   );
---
+++ src/Components/About/about.js
@@ -0,0 +1,11 @@
+import './about.css';
+
+const About = () => {
+    return (
+        <>
+            The Founders
+        </>
+    )
+}
+
+export default About
