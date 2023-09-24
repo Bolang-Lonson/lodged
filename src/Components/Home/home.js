b15363e61e5b2e759e91ec6c91ea8bb0a5b5748f
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-24 17:45:12 +0100

    curved the landing page header's bottom border

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -5,13 +5,17 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <>
-        <div className='header'>
+        <header className='header'>
             <Navbar/>
             <div className='info'>
                 <h1>let's find <br></br>your <br></br>dream hotel room or <br></br>guest house</h1>
                 <Link className='s-u'><button>get started &rarr;</button></Link>
             </div>
+        </header>
+        <div id='howto'>
+
         </div>
+        <div id='footer'></div>
         </>
     )
 }
