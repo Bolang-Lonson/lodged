471d9d115c19c8b7116635662f91b74245324b57
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-05 20:19:26 +0100

    updated login

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,13 +1,18 @@
 import './login.css';
+import logo from '../../Assets/Images/logo2-png.png';
 
 const Login = () => {
+    function signIn() {
+
+    }
+    
     return (
         <body>
             <form>
                 <img src={logo}/>
                 <input type='email'></input>
                 <input type='password'></input>
-                <button type='submit'>Login</button>
+                <button type='submit' onSubmit={signIn}>Login</button>
             </form>
         </body>
     )
