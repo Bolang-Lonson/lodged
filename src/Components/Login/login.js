62d8c834e408e3e13c83a283304e156848ae12af
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-11 07:40:19 +0100

    login page styled with bootstrap

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,5 +1,6 @@
 import './login.css';
-import logo from '../../Assets/Images/logo2-png.png';
+import logo from '../../Assets/Images/logo-char.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
     function signIn() {
@@ -7,14 +8,30 @@ const Login = () => {
     }
     
     return (
-        <body>
-            <form>
-                <img src={logo}/>
-                <input type='email'></input>
-                <input type='password'></input>
-                <button type='submit' onSubmit={signIn}>Login</button>
-            </form>
-        </body>
+        <div className='container'>
+            <div className='form_box'>
+                <form>
+                    <div>
+                        <img src={ logo } alt='...' className='mx-5'/>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">Enter a valid email address.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                    </div>
+                    <button type="submit" className="btn btn-outline-success px-5">Login</button>
+                    <div className='form-text pt-1'>Don't have an account? <a href='/signup'>Signup</a></div>
+                </form>
+            </div>
+        </div>
     )
 }
 
