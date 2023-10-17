f18bd22fa8c4a7aea4987354fbfae532c798c6f2
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-17 12:39:45 +0100

    implemented usetate to only signup when terms are agreed with

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,5 +1,7 @@
 import './login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import drklogo from '../../Assets/Images/other/logo-char.png'
+import { useState } from 'react';
 
 const Login = () => {
     function signIn() {
@@ -7,20 +9,21 @@ const Login = () => {
     }
     
     return (
-        <div className='container-fluid'>
+        <div>
+            <div className='logo-box pt-4'><img src={drklogo} alt='...'/></div>
             <div className='form_box'>
+                <h1 className="text-center form-group mb-3">Login</h1>
                 <form>
-                    <div className='login-logo'></div>
-                    <div className="mb-3">
+                    <div className="form-group mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         <div id="emailHelp" className="form-text">Enter a valid email address.</div>
                     </div>
-                    <div className="mb-3">
+                    <div className="form-group mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1"/>
                     </div>
-                    <div className="mb-3 form-check">
+                    <div className="form-group mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="rem-check"/>
                         <label className="form-check-label" htmlFor="rem-check">Remember me</label>
                     </div>
@@ -28,7 +31,7 @@ const Login = () => {
                     <div className='form-text pt-1'>Don't have an account? <a href='/signup'>Signup</a></div>
                 </form>
             </div>
-            <div><p className="mt-5 mb-3 text-body-secondary">&copy; Lodged.com 2024</p></div>
+            <div><p className="mt-5 form-group mb-3 text-body-secondary">&copy; Lodged.com 2024</p></div>
         </div>
     )
 }
--- src/Components/Signup/signup.js
+++ src/Components/Signup/signup.js
@@ -1,24 +1,32 @@
 import './signup.css';
 import countries from './countrycodes.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import drklogo from '../../Assets/Images/other/logo-char.png'
+import { useState } from 'react';
 
 const Signup = () => {
 
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheck = (event) => {
+        setIsChecked(event.target.checked);
+    }
+
     return (
         <div>
+            <div className='logo-box pt-4'><img src={drklogo} alt='...'/></div>
             <div className="container-sm box">
                 <h1 className="text-center mb-3">Signup</h1>
                 <form action="/signup" method="post">
-                    <div className="form-group my-2">
+                    <div className="form-floating my-2">
                         <label htmlFor="name">Name</label>
                         <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name"/>
                     </div>
-                    <div className="form-group my-2">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email"/>
+                    <div className="form-floating my-2">
+                        <label htmlFor="emailDisabled">Email</label>
+                        <input type="email" className="form-control" id="emailDisabled" name="email" placeholder="Enter your email"/>
                     </div>
-                    <div className="form-group my-2 telly">
-                        <label htmlFor="phone">Telephone Number</label>
+                    <div className="form-floating my-2 telly">
                         <select id="country-selector" name="country" className='form-select'>
                             <option value="">Country Code</option>
                             {countries.map((country, index) => (
@@ -27,18 +35,25 @@ const Signup = () => {
                                 </option>
                             )
                             )}
-                        </select>        
-                        <input type="tel" className="form-control" id="phone" name="phone" placeholder="Enter your telephone number"/>
+                        </select>
+                        <div className='form-floating number'>
+                            <label htmlFor="phone">Telephone Number</label>
+                            <input type="tel" className="form-control" id="phone" name="phone" placeholder="Enter your telephone number"/>
+                        </div>
                     </div>
-                    <div className="form-group my-2">
+                    <div className="form-floating my-2">
                         <label htmlFor="password">Password</label>
                         <input type="password" className="form-control" id="password" name="password" placeholder="Enter your password"/>
                     </div>
-                    <div className="form-group my-2">
+                    <div className="form-floating my-2">
                         <label htmlFor="confirmPassword">Confirm Password</label>
                         <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password"/>
                     </div>
-                    <button type="submit" className="btn btn-primary mt-4">Create Account</button>
+                    <div className="form-check my-3">
+                        <input type="checkbox" className="form-check-input" id="rem-check" checked={isChecked} onChange={handleCheck}/>
+                        <label className="form-check-label" htmlFor="rem-check">I have read and agree to the <a href='/termsandcoditions'>terms and conditions</a></label>
+                    </div>
+                    <button type="submit" className="btn btn-primary mt-2" disabled={!isChecked}>Create Account</button>
                 </form>
             </div>
         </div>
