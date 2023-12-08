4403945fa9d3f6321fb19591e15e16a219b60f1f
Bolang-Lonson <bbeinyoh12@gmail.com>
2023-12-07 18:51:06 -0800

    switching most of the project to bootstrap. Working on landing page currently

--- src/Components/Home/HowTo/howto.js
+++ src/Components/Home/HowTo/howto.js
@@ -2,20 +2,20 @@ import './howto.css';
 
 const HowTo = () => {
     return(
-        <>
-        <div id='howto'>
-            <h2><u>how it works</u></h2>
-            <div className='steps'>Step 1: Signup</div>
-            <div className='steps'>Step 2: Find Lodging</div>
-            <div className='steps'>Step 3: Book Lodging</div>
+        <div className='container'>
+            <div id='howto' className='row'>
+                <h2 className='col-12 text-center '><u>how it works</u></h2>
+                <div className='steps col-12 col-lg-3'>Step 1: Signup</div>
+                <div className='steps col-12 col-lg-3'>Step 2: Find Lodging</div>
+                <div className='steps col-12 col-lg-3'>Step 3: Book Lodging</div>
+            </div>
+            <hr/>
+            <div id='partners'>
+                {/* A carousel with the logos of the major hotels we get to join. the most enticing list */}
+                <h2>We Partnered With</h2>
+                <div className='partner-carousel'> Partners Carousel </div>
+            </div>
         </div>
-        <hr/>
-        <div id='partners'>
-            {/* A carousel with the logos of the major hotels we get to join. the most enticing list */}
-            <h2>We Partnered With</h2>
-            <div className='partner-carousel'> Partners Carousel </div>
-        </div>
-        </>
     )
 }
 
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -6,23 +6,27 @@ import Footer from '../Footer/footer';
 
 const Home = () => {
     return (
-        <>
-        <header className='header'>
-            <Navbar className='navbar' signedIn={false}/>
-            <div className='carousel'>
-                {/* Caoursel will display various images of hotels, guest houses and resorts */}
-                <h3>Greeting Carousel</h3>
+        <div>
+            <div className='header'>
+                <Navbar className='navbar' signedIn={false}/>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='carousel col-12 col-lg-6'>
+                            {/* Caoursel will display various images of hotels, guest houses and resorts */}
+                            <h3>Greeting Carousel</h3>
+                        </div>
+                        <div className='info col-12 col-lg-4'>
+                            <h1>let's find <br></br>your <br></br>dream hotel <br></br>room or <br></br>guest house</h1>
+                            <Link className='s-u' to='/signup'><button>get started &rarr;</button></Link>
+                            <p>Already have an account? <a href='/login'>Login</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='info'>
-                <h1>let's find <br></br>your <br></br>dream hotel <br></br>room or <br></br>guest house</h1>
-                <Link className='s-u' to='/signup'><button>get started &rarr;</button></Link>
-                <p>Already have an account? <a href='/login'>Login</a></p>
-            </div>
-        </header>
-        {/* HowTo is separated into its own component because it will become more complicated in the future */}
-        <HowTo/>
-        <Footer/>
-        </>
+            {/* HowTo is separated into its own component because it will become more complicated in the future */}
+            <HowTo/>
+            <Footer/>
+        </div>
     )
 }
 
