f7a881787b8e376ba53e07896d0accbb12a41463
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-03-04 09:25:05 +0100

    implementing bootstrap tooltips

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/home';
 import About from './Components/About/about';
