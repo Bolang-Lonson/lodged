fdcb79fb00b2dc86fe6c9c007c90a2ea65b5b017
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-04-08 15:45:28 +0100

    Created a separate modal.jsx file to make a costumisable modal component with dynamic content

---
+++ src/Components/Signup/modal.jsx
@@ -0,0 +1,31 @@
+const Modal = (props) => {
+
+    const sizeClass = (props.id === 'terms-n-conditions')? 'modal-lg': '';
+    const scroll = (props.scrollable === true)? 'modal-dialog-scrollable': '';
+
+	return(
+		<div className="modal fade" id={props.id}
+			tabIndex={-1} 
+			aria-labelledby='modalLabel' aria-hidden='true'
+			data-bs-backdrop='static'
+			data-bs-keyboard='false'
+		>
+			<div className={`modal-dialog ${sizeClass} modal-dialog-centered ${scroll}`}>
+				<div className="modal-content bg-light rounded-4">
+					<div className="modal-header border-bottom-0">
+						<h4 className="modal-title w-100 d-block text-center" id='modalLabel'>{props.title}</h4>
+						<button className="btn-close" type='button' data-bs-dismiss='modal' aria-label='close'></button>
+					</div>
+					<div className="modal-body">
+						{props.body()}
+					</div>
+                    <div className="modal-footer border-top-0">
+                        {props.footer? props.footer(): ''}
+                    </div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal
